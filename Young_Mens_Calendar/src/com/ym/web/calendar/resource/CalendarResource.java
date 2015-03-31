5db38e84546623db910ddc2e1e372c7d101d4786
Jacob Ransom <jacob.ransom@enmu.edu>
2015-03-31 16:52:33 -0700

    Added a couple more methods for handling calendar objects.

--- Young_Mens_Calendar/src/com/ym/web/calendar/resource/CalendarResource.java
+++ Young_Mens_Calendar/src/com/ym/web/calendar/resource/CalendarResource.java
@@ -34,12 +34,13 @@ public class CalendarResource {
      * Method for handling a post calendar
      * 
      * @param request
-     * @return
+     * @param calendar
+     * @return Response
      */
     @POST
     @Consumes({ MediaType.APPLICATION_JSON })
     @Produces({ MediaType.APPLICATION_JSON })
-    public Response postRequestMessage(@Context HttpServletRequest request, Calendar calendar) {
+    public Response postCalendar(@Context HttpServletRequest request, Calendar calendar) {
 
         ResponseBuilder builder = Response.status(Status.OK).entity("Post Successful").type(
             MediaType.APPLICATION_JSON_TYPE);
@@ -63,9 +64,13 @@ public class CalendarResource {
         return builder.build();
     }
 
+    /**
+     * @param request
+     * @return Response
+     */
     @GET
     @Produces({ MediaType.APPLICATION_JSON })
-    public Response getRequestList(@Context HttpServletRequest request) {
+    public Response getCalendarList(@Context HttpServletRequest request) {
 
         ArrayList<Calendar> result = new ArrayList<Calendar>();
         ResponseBuilder builder = Response.status(Status.OK).entity(result).type(MediaType.APPLICATION_JSON_TYPE);
@@ -104,11 +109,77 @@ public class CalendarResource {
 
         EntityManager em = MyEntityManagerFactory.createEntityManager();
         try {
-            Query queryMessages = em.createQuery("SELECT OBJECT(ca) FROM Calendar ca WHERE calendarId = " + calendarId);
+            Query queryMessages = em.createQuery("SELECT OBJECT(ca) FROM Calendar ca WHERE calendarId = :calendarId");
+            queryMessages.setParameter("calendarId", calendarId);
             Calendar calendar = (Calendar) queryMessages.getSingleResult();
             if (calendar != null) {
                 builder.entity(calendar);
             }
+            em.close();
+        } catch (Throwable th) {
+            builder = Response.status(Status.INTERNAL_SERVER_ERROR).entity(th.getMessage()).type(
+                MediaType.APPLICATION_JSON_TYPE);
+        } finally {
+            if (em != null && em.isOpen()) {
+                em.close();
+            }
+        }
+
+        return builder.build();
+    }
+
+    /**
+     * @param request
+     * @param calendarId
+     * @return Response
+     */
+    @GET
+    @Path("{calendarId : \\d+}/delete")
+    @Produces({ MediaType.APPLICATION_JSON })
+    public Response deleteCalendarById(@Context HttpServletRequest request, @PathParam("calendarId") int calendarId) {
+        ResponseBuilder builder = Response.status(Status.OK).entity("Delete Successful").type(
+            MediaType.APPLICATION_JSON_TYPE);
+
+        EntityManager em = MyEntityManagerFactory.createEntityManager();
+        try {
+            EntityTransaction trans = em.getTransaction();
+            trans.begin();
+            Query queryDeleteCalendar = em.createQuery("DELETE FROM Calendar ca WHERE calendarId = :calendarId");
+            queryDeleteCalendar.setParameter("calendarId", calendarId);
+            queryDeleteCalendar.executeUpdate();
+            trans.commit();
+            em.close();
+        } catch (Throwable th) {
+            builder = Response.status(Status.INTERNAL_SERVER_ERROR).entity(th.getMessage()).type(
+                MediaType.APPLICATION_JSON_TYPE);
+        } finally {
+            if (em != null && em.isOpen()) {
+                em.close();
+            }
+        }
+
+        return builder.build();
+    }
+
+    /**
+     * @param request
+     * @param calendar
+     * @return Response
+     */
+    @POST
+    @Consumes({ MediaType.APPLICATION_JSON })
+    @Produces({ MediaType.APPLICATION_JSON })
+    public Response updateCalendar(@Context HttpServletRequest request, Calendar calendar) {
+
+        ResponseBuilder builder = Response.status(Status.OK).entity("Update Successful").type(
+            MediaType.APPLICATION_JSON_TYPE);
+
+        EntityManager em = MyEntityManagerFactory.createEntityManager();
+        try {
+            EntityTransaction trans = em.getTransaction();
+            trans.begin();
+            em.merge(calendar);
+            trans.commit();
             em.close();
         } catch (Throwable th) {
             builder = Response.status(Status.INTERNAL_SERVER_ERROR).entity(th.getMessage()).type(
